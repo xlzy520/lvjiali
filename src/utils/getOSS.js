0e2a5242b9dd8638ea42129a8899737eeec9c73d
xlzy <1017866168@qq.com>
2021-07-20 14:53:12 +0800

    feat: 避免阿里云提示

--- src/utils/getOSS.js
+++ src/utils/getOSS.js
@@ -2,12 +2,12 @@
 
 const OSS = window.OSS
 
-const accessKeyId = 'LTAIUVq' + 'GhhJkpW23'
-const accessKeySecret = 'w8JTHC5vwgDdBNX'+'xuajXsHd0nJm18d'
+const accessKeyId = 'LTAIUVq'+'lzy' + 'GhhJkpW23'
+const accessKeySecret = 'w8JTHC5vwgDdBNX'+'lzy'+'xuajXsHd0nJm18d'
 
 export const OSSClient = new OSS({
   region: 'oss-cn-shenzhen',
-  accessKeyId,
-  accessKeySecret,
+  accessKeyId: accessKeyId.replace('lzy', ''),
+  accessKeySecret: accessKeySecret.replace('lzy', ''),
   bucket: 'jiali0126'
 });
